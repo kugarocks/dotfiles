67ae6382cbdad1d4acc94fc0b597f22b67e0cd53
kugarocks <kugacola@gmail.com>
2024-12-23 01:16:23 +0800

    oh yeah

--- hammerspoon/Spoons/AppActivator.spoon/init.lua
+++ hammerspoon/Spoons/AppActivator.spoon/init.lua
@@ -1,6 +1,8 @@
 --- === AppActivator ===
 ---
---- Activate focused window app such as raycast, chatgpt, etc.
+--- A Spoon that allows you to activate/focus applications using their own hotkeys.
+--- When the configured hotkey is pressed while its corresponding app is focused,
+--- it will bring that app to front.
 
 local obj = {}
 obj.__index = obj
@@ -13,59 +15,89 @@ obj.homepage = "https://github.com/kugarocks/dotfiles"
 obj.license = "MIT - https://opensource.org/licenses/MIT"
 
 -- Internal variables
-obj.bundleIdMap = {}
 obj.eventTap = nil
+obj.hotkeyToBundleIDMap = {}
 
--- Get current application's bundle ID
-function obj:getFocusedAppBundleID()
-    local bundleID
-    local currentWindow = hs.window.focusedWindow()
-    
-    if currentWindow then
-        local windowApp = currentWindow:application()
-        if windowApp then
-            bundleID = windowApp:bundleID()
-        end
+-- Generate a unique key for modifier+key combination
+-- If there's only one modifier, return it directly with the key
+-- If there are multiple modifiers, sort them first to ensure consistent matching
+function obj:_generateMapKey(modifiers, key)
+    -- Skip sorting if there's only one modifier
+    if #modifiers == 1 then
+        return modifiers[1] .. "+" .. key
     end
-
-    return bundleID
+    
+    -- Sort modifiers only when there are multiple
+    local mods = {}
+    for _, mod in ipairs(modifiers) do
+        table.insert(mods, mod)
+    end
+    table.sort(mods)
+    return table.concat(mods, "+") .. "+" .. key
 end
 
---- AppActivator:setBundleIds(ids)
+--- AppActivator:bindHotkeys(ids)
 --- Method
---- Set the bundle IDs of applications to monitor
+--- Configures hotkey to bundle ID mappings for application activation.
 ---
 --- Parameters:
----  * ids - An array of bundle IDs, e.g., {"com.raycast.macos"}
-function obj:setBundleIds(ids)
-    local map = {}
-    for _, id in ipairs(ids) do
-        map[id] = true
+---  * ids - A table mapping bundle IDs to their hotkey configurations
+---
+--- Example:
+---  ```lua
+---  spoon.AppActivator:bindHotkeys({
+---     ["com.raycast.macos"] = {{"cmd"}, "space"},
+---     ["com.another.app"] = {{"cmd", "shift"}, "space"}
+---  })
+---  ```
+function obj:bindHotkeys(ids)
+    -- Create lookup table for mapping hotkey combinations to bundle IDs
+    self.hotkeyToBundleIDMap = {}
+    for bundleID, keyConfig in pairs(ids) do
+        local mapKey = self:_generateMapKey(keyConfig[1], keyConfig[2])
+        self.hotkeyToBundleIDMap[mapKey] = bundleID
     end
-    self.bundleIdMap = map
 end
 
 --- AppActivator:start()
 --- Method
---- Start monitoring
+--- Starts monitoring keyboard events for configured hotkeys.
+--- When a matching hotkey is pressed while its corresponding app is focused,
+--- that app will be brought to front.
 ---
 --- Returns:
----  * The AppActivator object for method chaining
+---  * The AppActivator object
 function obj:start()
     if self.eventTap then
         self.eventTap:stop()
     end
     
-    self.eventTap = hs.eventtap.new({hs.eventtap.event.types.flagsChanged}, function(event)
-        local bundleID = self:getFocusedAppBundleID()
-        
-        if self.bundleIdMap[bundleID] then
-            local app = hs.application.get(bundleID)
-            if app then
-                app:activate()
+    self.eventTap = hs.eventtap.new({hs.eventtap.event.types.flagsChanged, hs.eventtap.event.types.keyDown}, function(event)
+        local flags = event:getFlags()
+        local keyCode = event:getKeyCode()
+        local pressedKey = hs.keycodes.map[keyCode]
+
+        local activeModifiers = {}
+        for mod, isPressed in pairs(flags) do
+            if isPressed then
+                table.insert(activeModifiers, mod)
             end
         end
-        
+
+        local mapKey = self:_generateMapKey(activeModifiers, pressedKey)
+        local bundleID = self.hotkeyToBundleIDMap[mapKey]
+
+        if not bundleID then
+            return false
+        end
+
+        local app = hs.application.get(bundleID)
+        if app then
+            app:activate()
+        else
+            hs.application.launchOrFocusByBundleID(bundleID)
+        end
+
         return false
     end):start()
     
--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -1,7 +1,7 @@
--- osascript -e 'id of app "Raycast"'
-local bundleIds = {
-    "com.raycast.macos",
-}
+local raycastBundleID = "com.raycast.macos"
+
 hs.loadSpoon("AppActivator")
-spoon.AppActivator:setBundleIds(bundleIds)
+spoon.AppActivator:bindHotkeys({
+    [raycastBundleID] = {{"cmd"}, "space"},
+})
 spoon.AppActivator:start()
